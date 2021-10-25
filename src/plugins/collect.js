63902bf2df4705e7153b437f519d7f988b66534f
Ultra Cookie <59873833+Error-Cookie@users.noreply.github.com>
2021-10-25 21:45:34 +0200

    Collect item

---
+++ src/plugins/collect.js
@@ -0,0 +1,31 @@
+const { owner } = require('../../config');
+const mineflayer = require('mineflayer');
+
+module.exports = bot => {
+    let mcdata;
+
+    bot.once('spawn', () => {
+        mcdata = require('minecraft-data')(bot.version);
+    });
+
+    bot.on('chat', (name, msg) => {
+        const args = msg.split(' ');
+        if (args[0] != 'collect' || name != owner) return;
+
+        const targetBlock = mcdata.blocksByName[args[1]];
+        if (!targetBlock) return bot.chat('Block does not exist!');
+
+        bot.chat('Collecting ' + args[1] + '!');
+
+        const block = bot.findBlock({
+            matching: targetBlock.id,
+            maxDistance: 64
+        });
+
+        if (!block) return bot.chat('Block not in range!');
+
+        bot.collectBlock.collect(block, err => {
+            if (err) bot.chat(err.message);
+        });
+    });
+}
