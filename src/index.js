36e64125ba252f2f60439b2b3155fa61313d24b9
Ultra Cookie <59873833+Error-Cookie@users.noreply.github.com>
2021-10-26 15:33:27 +0200

    Update IntelliSense

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const mineflayer = require('mineflayer');
 const pathfinder = require('mineflayer-pathfinder').pathfinder;
 const collect = require('mineflayer-collectblock').plugin;
 
+/**
+ * @param {mineflayer.Bot} bot 
+ */
 function loadPlugins(bot) {
     const dir = path.join(__dirname, 'plugins');
     const modules = fs
