fbab380fc3924ab202159f031d1d17107bc9665d
Ultra Cookie <59873833+Error-Cookie@users.noreply.github.com>
2021-10-28 16:51:08 +0200

    Clean

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ const Bot = require('../lib/Bot');
 const bot = new Bot();
 
 bot.on('ready', () => {
-    bot.followNearestEntity('player');
-    for (let i = 0; i < 100; i++) {
-        bot.bot.chat('d');
-    }
+    
 });
