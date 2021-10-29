28e0faa2766635053ab4b1f5a735b345efd1a7ff
Ultra Cookie <59873833+Error-Cookie@users.noreply.github.com>
2021-10-29 17:38:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const Bot = require('../lib/Bot');
 
 const bot = new Bot();
 
-bot.on('ready', () => {
-    
+bot.on('ready', async () => {
+    for (let i = 0; i < 10; i++) {
+        await bot.kill(bot.bot.nearestEntity(e => e.name == 'player')).then(() => console.log('done'));
+    }
 });
